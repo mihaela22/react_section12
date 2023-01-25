f030034a92ccb5aa7a67d57000f4b566c7240835
unknown <mihaela@matrixinformatika.hr>
2023-01-25 14:19:08 +0100

    useCallback & state updates & scheduling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowTogle] = useState(false);
 
   console.log("APP RUNNING");
 
-  const toggleParagrapHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const toggleParagrapHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+  const allowToggleHandler = () => {
+    setAllowTogle(true);
   };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
       <Button onClick={toggleParagrapHandler}>Toggle Paragraph!</Button>
     </div>
   );
